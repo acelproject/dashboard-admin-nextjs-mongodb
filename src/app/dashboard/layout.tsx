604d4456e98c8468cff3aa70f56c31a124ea7ee1
acelproject <aclprcjt@gmail.com>
2024-04-23 13:10:55 +0800

    fix bg-color responsive mobile

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex w-full bg-bg">
       <div
-        className={`h-[100vh]  no-scrollbar overflow-scroll transition-all fixed z-50 md:bg-bgSoft bg-slate-900 my-0 p-4 md:p-4 ${closeSidebar? "translate-x-[0px] md:translate-x-[-300px]":"translate-x-[-300px] md:translate-x-[0px]"}   w-[80%]  md:w-[20%]`}
+        className={`h-[100vh]  no-scrollbar overflow-scroll transition-all fixed z-50 md:bg-bgSoft bg-slate-900  my-0 p-4 md:p-4 ${closeSidebar? "translate-x-[0px] md:translate-x-[-300px]":"translate-x-[-350px] md:translate-x-[0px]"}   w-[80%]  md:w-[20%]`}
       >
         {/* {closeSidebar ? null : (
         )} */}
@@ -19,7 +19,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         />
         
       </div>
-      <div className={` p-4 transition-all ${closeSidebar?"md:w-[100%]":"w-[100%] md:w-[80%] "}  right-0 absolute`}>
+      <div className={` p-4 transition-all ${closeSidebar?"md:w-[100%]":"w-[100%] md:w-[80%] "} right-0 absolute`}>
         <Navbar setCloseSidebar={setCloseSidebar} closeSidebar={closeSidebar} />
         {children}
         <Footer />
